0bd6b3af4014448c19d6fe2bb6cb89b648cb1fba
Ramon Andrade <124772095+ramonsaguini@users.noreply.github.com>
2023-12-28 10:01:36 +0000

    fixed error ckick btn clear

--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -1,17 +1,21 @@
-function displayHighScores() {
-  const highScoresContainer = document.querySelector('#high-scores');
-  const scores = JSON.parse(localStorage.getItem('scores')) || [];
-  scores.forEach(score => {
-    const scoreItem = document.createElement('li');
-    scoreItem.textContent = `${score.playerName}: Correct Answers: ${score.score} .  Time: ${score.time} seconds - ${score.message}`;
-    highScoresContainer.appendChild(scoreItem);
-  });
-}
-window.onload = displayHighScores;
+document.addEventListener("DOMContentLoaded", function () {
+  function displayHighScores() {
+    const highScoresContainer = document.querySelector('#high-scores');
+    const scores = JSON.parse(localStorage.getItem('scores')) || [];
+    scores.forEach(score => {
+      const scoreItem = document.createElement('li');
+      scoreItem.textContent = `${score.playerName}: Correct Answers: ${score.score} .  Time: ${score.time} seconds - ${score.message}`;
+      highScoresContainer.appendChild(scoreItem);
+    });
+  }
 
-function clearScores() {
-  localStorage.clear();
-  window.location.reload()
-}
-let clearBtn = document.querySelector("#remove");
-clearBtn.addEventListener("click", clearScores);
+  window.onload = displayHighScores;
+
+  function clearScores() {
+    localStorage.clear();
+    window.location.reload();
+  }
+
+  let clearBtn = document.querySelector("#remove");
+  clearBtn.addEventListener("click", clearScores);
+});
